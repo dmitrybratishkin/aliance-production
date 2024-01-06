1ccfd751016157f8bdd7517f9113f4abfaa0a1be
dmitrybratishkin <bygeorg2623@gmail.com>
2024-01-06 20:27:57 +0300

    add: swiper

--- js/main.js
+++ js/main.js
@@ -34,3 +34,57 @@ mMenuToggle.addEventListener("click", (event) => {
     event.preventDefault();
     menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    autoHeight: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+            slidesPerView: 2,
+            centeredSlides: true,
+            slidesOffsetBefore: -120,
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+    },
+});
+
+const swiperSteps = new Swiper('.steps-slider', {
+    speed: 400,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.steps-button-next',
+        prevEl: '.steps-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+            slidesPerView: 2,
+            centeredSlides: true,
+            slidesOffsetBefore: -120,
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 4,
+        },
+    }
+});
